461bef7a704afc2bf8d74bb6dd2b810c45714086
datatags <datatags@users.noreply.github.com>
2021-03-11 15:50:20 -0800

    v6.8.2 - 1.8 carrots/potatoes support

--- src/me/Datatags/CommandMineRewards/state/LegacyStateManager.java
+++ src/me/Datatags/CommandMineRewards/state/LegacyStateManager.java
@@ -1,5 +1,9 @@
 package me.Datatags.CommandMineRewards.state;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.CropState;
 import org.bukkit.Material;
 import org.bukkit.NetherWartsState;
@@ -10,20 +14,21 @@ import org.bukkit.material.NetherWarts;
 
 @SuppressWarnings("deprecation")
 public class LegacyStateManager implements StateManager {
-
+	private static final Set<Material> CROPS = new HashSet<>(Arrays.asList(Material.POTATO, Material.CARROT, Material.valueOf("CROPS"), Material.valueOf("NETHER_WARTS")));
 	@Override
 	public boolean isFullGrown(BlockState state) {
 		MaterialData data = state.getData();
-		if (data instanceof NetherWarts) {
-			return ((NetherWarts)data).getState() == NetherWartsState.RIPE;
-		} else {
+		if (data instanceof Crops) {
 			return ((Crops)data).getState() == CropState.RIPE;
+		} else if (data instanceof NetherWarts) {
+			return ((NetherWarts)data).getState() == NetherWartsState.RIPE;
 		}
+		return state.getRawData() == 7; // seems to be the only way of checking for fully-grown carrots/potatoes in 1.8
 	}
 
 	@Override
 	public boolean canHaveData(Material mat) {
-		return Crops.class.isAssignableFrom(mat.getData()) || NetherWarts.class.isAssignableFrom(mat.getData());
+		return CROPS.contains(mat);
 	}
 
 }
